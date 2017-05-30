47064716db3f4aeae6414837db402134e92582ad
신우성 <lisa960310@naver.com>
2017-05-30 10:21:11 +0900

    과목 관리 시작~ 엑셀 열기와 메인 메뉴 출력까지만 완성

---
+++ schedule/schedule/FindLecture.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace schedule
+{
+    public class FindLecture
+    {
+        Excel.Application excelApplication;
+        Excel.Workbook excelWorkbook;
+        Excel._Worksheet excelWorkSheet;
+        Excel.Range excelRange;
+        int rowCount; //
+        int colCount;
+
+        public FindLecture(string directory)
+        {
+            excelApplication = new Excel.Application(); // Excel 첫번째 워크시트 가져오기
+            excelWorkbook = excelApplication.Workbooks.Open(directory);
+            excelWorkSheet = excelWorkbook.Sheets[1];
+            excelRange = excelWorkSheet.UsedRange;
+
+            rowCount = excelRange.Rows.Count;
+            colCount = excelRange.Columns.Count;
+        }
+        ~FindLecture()
+        {
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            //release com objects to fully kill excel process from running in the background
+            Marshal.ReleaseComObject(excelRange);
+            Marshal.ReleaseComObject(excelWorkSheet);
+
+            //close and release
+            excelWorkbook.Close();
+            Marshal.ReleaseComObject(excelWorkbook);
+
+            //quit and release
+            excelApplication.Quit();
+            Marshal.ReleaseComObject(excelApplication);
+        }
+
+        public void initExcel()
+        {
+            Console.WriteLine(rowCount);
+            Console.WriteLine(colCount);
+        }
+        public void searchLecture(string nLecture)
+        {
+
+            for (int i = 1; i <= rowCount; i++)
+            {
+
+                if (excelRange.Cells[i, 2].Value2.ToString() != nLecture)
+                {
+                    continue;
+                }
+
+                for (int j = 1; j <= colCount; j++)
+                {
+                    //new line
+                    //  if (j == 1)
+                    //  Console.Write("\r\n");
+
+                    //write the value to the console
+                    if (excelRange.Cells[i, j] != null && excelRange.Cells[i, j].Value2 != null)
+                        Console.Write(excelRange.Cells[i, j].Value2.ToString() + "\t ");
+
+                }
+                Console.WriteLine("\r");
+            }
+
+        }
+    }
+}
---
+++ schedule/schedule/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using schedule;
+
+namespace Sandbox
+{
+    public class Read_From_Excel
+    {
+        public static void Main()
+        {
+            Console.SetWindowSize(180, 50); // 콘솔 창 사이즈 설정
+
+            FindLecture findLecture = new FindLecture(@"C:\Users\신우성\Desktop\excelStudy.xlsx"); // 엑셀 파일 위치
+            findLecture.initExcel(); // 엑셀을 열고 사용할 준비 단계
+
+            findLecture.searchLecture("컴퓨터공학과");
+
+            findLecture.searchLecture("디지털공학과");
+
+
+
+        }
+    }
+}
